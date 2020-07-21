85122c252000ef54a4b3aa9254ca1e36aa19e103
shivam74662 <shivam.thombre@active.ai>
2020-07-21 12:31:53 +0530

    age calculator

---
+++ assignments-and-exercises/js-exercise/age-calc.js
@@ -0,0 +1,7 @@
+var age = prompt("How old are you ?");
+
+var days = age * 365.25;
+
+alert(age + " years is roughly " + days + " days");
+
+console.log(age + " years is roughly " + days + " days");
