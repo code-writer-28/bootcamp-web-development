4e80c8c291f8085cd6f2bc6abbb4e9785d40ff15
shivam74662 <shivam.thombre@active.ai>
2020-07-21 11:58:07 +0530

    js-basics-exercise

---
+++ assignments-and-exercises/js-exercise/base.js
@@ -0,0 +1,11 @@
+var firstName = prompt("What is your First Name?");
+
+var lastName = prompt("What is your Last Name?");
+
+var age = prompt("What is your age?");
+
+var fullName = firstName + " " + lastName;
+
+console.log("Your full name is " + fullName);
+
+console.log("Your are " + age + " years old");
