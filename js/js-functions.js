b9c59fb23d424fb55a3b22d97ab8f10b2a75391e
shivam74662 <shivam.thombre@active.ai>
2020-07-26 00:33:19 +0530

    js functions

---
+++ js/js-functions.js
@@ -0,0 +1,17 @@
+function sayHello() {
+    console.log("Say Hello!");
+}
+
+sayHello();
+
+function sayHi(name) {
+    console.log("Say Hi to " + name);
+}
+
+sayHi("Shivam");
+
+function area(length, breadth) {
+    console.log("The area of Rectangle is " + length * breadth + " m^2");
+}
+
+area(21, 12);
