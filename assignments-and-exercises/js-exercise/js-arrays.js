d04b41a1ce247833ca87e0c76e3b130a2b8a60f9
shivam74662 <shivam.thombre@active.ai>
2020-07-26 23:25:57 +0530

    js arrays

--- assignments-and-exercises/js-exercise/js-arrays.js
+++ assignments-and-exercises/js-exercise/js-arrays.js
@@ -9,3 +9,28 @@ var friendsGroups = [
     ["sonu", "monu", "vinu"]
 ];
 console.log(friendsGroups[2][0]);
+
+// Exercise 3
+var todos = ["satam"];
+var input = prompt("What you like to do?");
+
+while (input !== "quit") {
+    if (input === "list") {
+        console.log(todos);
+    } else if (input === "new") {
+        var newToDo = prompt("Enter new To Do");
+        todos.push(newToDo);
+    }
+    input = prompt("What you like to do?");
+}
+console.log("QUIT");
+
+//Exercise 4
+var nums = [1, 2, 4, 5, 6, 7, 8, 9, 10, 4, 3, 235, 2, 234, 234, 25212, 2354, 2366, 23, 232, 45234];
+var colorss = ["red", "blues"];
+
+nums.forEach(function (color) {
+    if (color % 3 === 0) {
+        console.log(color);
+    }
+});
