08377e5df41c6bf8726fc65ad6d99c4611d1920a
shivam74662 <shivam.thombre@active.ai>
2020-07-26 22:49:57 +0530

    js arrays

---
+++ assignments-and-exercises/js-exercise/js-arrays.js
@@ -0,0 +1,11 @@
+// Exercise 1
+var numbers = [22, 12, 34, 35, 12, 34];
+console.log(numbers[numbers.length - 1]);
+
+// Exercise 2
+var friendsGroups = [
+    ["satyam", "shivam", "sundaram"],
+    ["preeti", "prakash", "thombre"],
+    ["sonu", "monu", "vinu"]
+];
+console.log(friendsGroups[2][0]);
