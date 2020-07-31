c7423914a3b9e1dc28120478769d2e4be1d16a5e
shivam74662 <shivam.thombre@active.ai>
2020-07-31 12:05:07 +0530

    array problem set

--- assignments-and-exercises/js-exercise/js-arrays.js
+++ assignments-and-exercises/js-exercise/js-arrays.js
@@ -1,14 +1,14 @@
 // // Exercise 1
-// var numbers = [22, 12, 34, 35, 12, 34];
-// console.log(numbers[numbers.length - 1]);
+var numbers = [22, 12, 34, 35, 12, 34];
+console.log(numbers[numbers.length - 1]);
 
 // // Exercise 2
-// var friendsGroups = [
-//     ["satyam", "shivam", "sundaram"],
-//     ["preeti", "prakash", "thombre"],
-//     ["sonu", "monu", "vinu"]
-// ];
-// console.log(friendsGroups[2][0]);
+var friendsGroups = [
+    ["satyam", "shivam", "sundaram"],
+    ["preeti", "prakash", "thombre"],
+    ["sonu", "monu", "vinu"]
+];
+console.log(friendsGroups[2][0]);
 
 // Exercise 3
 var todos = ["satyam"];
@@ -48,11 +48,11 @@ function deleteItem() {
 }
 
 //Exercise 4
-// var nums = [1, 2, 4, 5, 6, 7, 8, 9, 10, 4, 3, 235, 2, 234, 234, 25212, 2354, 2366, 23, 232, 45234];
-// var colorss = ["red", "blues"];
+var nums = [1, 2, 4, 5, 6, 7, 8, 9, 10, 4, 3, 235, 2, 234, 234, 25212, 2354, 2366, 23, 232, 45234];
+var colorss = ["red", "blues"];
 
-// nums.forEach(function (color) {
-//     if (color % 3 === 0) {
-//         console.log(color);
-//     }
-// });
+nums.forEach(function (color) {
+    if (color % 3 === 0) {
+        console.log(color);
+    }
+});
