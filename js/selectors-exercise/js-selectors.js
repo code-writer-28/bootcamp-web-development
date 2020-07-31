47b981a9386043456c7a3a13a38612e337892d94
shivam74662 <shivam.thombre@active.ai>
2020-07-31 19:11:44 +0530

    js selectors

---
+++ js/selectors-exercise/js-selectors.js
@@ -0,0 +1,14 @@
+console.log("document.getElementById()");
+console.log(document.getElementById("first"));
+
+console.log("document.getElementsByClassName()");
+console.log(document.getElementsByClassName("special"))
+
+console.log("document.querySelector()");
+console.log(document.querySelector("#first"));
+
+console.log("document.querySelectorAll()");
+console.log(document.querySelectorAll(".special"));
+
+console.log("document.getElementsByTagName()");
+console.log(document.getElementsByTagName("h1"))
