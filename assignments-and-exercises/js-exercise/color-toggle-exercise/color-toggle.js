ac1932743b8e5745004aa1c7d1ac06a037e7d1cc
shivam74662 <shivam.thombre@active.ai>
2020-08-01 14:42:02 +0530

    indexing

--- assignments-and-exercises/js-exercise/color-toggle-exercise/color-toggle.js
+++ assignments-and-exercises/js-exercise/color-toggle-exercise/color-toggle.js
@@ -6,10 +6,9 @@ button.addEventListener("click", function () {
     if (isBlack) {
         document.querySelector("body").style.background = "whitesmoke";
         document.querySelector("h1").style.color = "black";
-        isBlack = false;
     } else {
         document.querySelector("body").style.background = "black";
         document.querySelector("h1").style.color = "whitesmoke";
-        isBlack = true;
     }
+    isBlack = !isBlack;
 });
