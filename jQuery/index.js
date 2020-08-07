670c437f6ea369f56d387eca788f80744cfda80d
shivam74662 <shivam.thombre@active.ai>
2020-08-07 17:48:50 +0530

    jquery started

---
+++ jQuery/index.js
@@ -0,0 +1,11 @@
+$("div").css({
+    "backgroundColor": "purple"
+});
+
+$("div.highlight").css("width", "200px");
+
+$("div#third").css({
+    "border": "5px solid orange"
+});
+
+$("div:first-of-type").css("color", "white");
