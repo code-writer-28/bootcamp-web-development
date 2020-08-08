11a447f147f9b7b034413ae3ca3f975783ec8dd6
shivam74662 <shivam.thombre@active.ai>
2020-08-08 10:36:25 +0530

    updating index

---
+++ index.js
@@ -0,0 +1,81 @@
+$("h1").on("mouseover", function () {
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+});
+
+$("h3").on("mouseover", function () {
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+
+});
+
+$(".navbar-brand").on("mouseover", function () {
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+    $(this).fadeToggle("5000", function () {
+        $(this).toggleClass("selected");
+    });
+});
